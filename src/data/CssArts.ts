18744f33e660ef08d5da3955680bd9568d3a5ccc
Jojo25011990 <Jojo25011990@gmail.com>
2026-03-08 16:17:26 +0100

    update images

--- src/data/CssArts.ts
+++ src/data/CssArts.ts
@@ -1,55 +1,65 @@
+import ferbPhineas from "../assets/Ferb-Phineas.png";
+import bomberman from "../assets/Bomberman.png";
+import pandaHearts from "../assets/Panda-Hearts.png";
+import mickeyMouse from "../assets/Mickey-Mouse.png";
+import cuteCat from "../assets/Cute-Cat-Face.png";
+import shadowCat from "../assets/shadow-cat.png";
+import houseCarBeaver from "../assets/house-car-beaver.png";
+import batman from "../assets/Batman.png";
+import shipSharkFish from "../assets/Ship-Shark-Fish.png";
+
 export const cssArts = [
 	{
 		id: 1,
-		src: "src/assets/Ferb-Phineas.png",
+		src: ferbPhineas,
 		title: "Ferb Fletcher and Phineas Flynn CSS Art",
 		alt: "Ferb character CSS art",
 	},
 	{
 		id: 2,
-		src: "src/assets/Bomberman.png",
+		src: bomberman,
 		title: "Bomberman CSS Art",
 		alt: "Bomberman CSS art",
 	},
 	{
 		id: 3,
-		src: "src/assets/Panda-Hearts.png",
+		src: pandaHearts,
 		title: "Panda with Hearts CSS Art",
 		alt: "Panda CSS art with hearts",
 	},
 	{
 		id: 4,
-		src: "src/assets/Mickey-Mouse.png",
+		src: mickeyMouse,
 		title: "Mickey Mouse Face CSS Art",
 		alt: "Mickey Mouse CSS art",
 	},
 	{
 		id: 5,
-		src: "src/assets/Cute-Cat-Face.png",
+		src: cuteCat,
 		title: "Cute Cat Face CSS Art",
 		alt: "Cute cat CSS art",
 	},
 	{
 		id: 6,
-		src: "src/assets/shadow-cat.png",
+		src: shadowCat,
 		title: "Shadow Cat CSS Art",
 		alt: "Shadow cat CSS art",
 	},
 	{
 		id: 7,
-		src: "src/assets/house-car-beaver.png",
+		src: houseCarBeaver,
 		title: "House, Car and Beaver CSS Art Scene",
 		alt: "House and car CSS scene",
 	},
 	{
 		id: 8,
-		src: "src/assets/Batman.png",
+		src: batman,
 		title: "Batman city with logo CSS Art Scene",
 		alt: "Batman CSS art",
 	},
 	{
 		id: 9,
-		src: "src/assets/Ship-Shark-Fish.png",
+		src: shipSharkFish,
 		title: "Ship, Shark and Fish CSS Art Scene",
 		alt: "Shark with ship CSS art",
 	},
